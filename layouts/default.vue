a721d70508fa9990b150d27abe9570a614a7c112
HDenizD <denizderezzed@gmail.com>
2024-05-03 09:55:41 +0200

    layout and index page

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="relative d-flex flex-column h-screen">
+    <header>header</header>
+    <main class="flex-grow-1">
+      <slot />
+    </main>
+    <footer>footer</footer>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Index Main Content</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

b86db4e671fc3cbe2f622946f09f8f91a6584f03
HDenizD <denizderezzed@gmail.com>
2024-05-03 09:41:08 +0200

    added custom theme

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,10 +2,27 @@
 import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+
+const customTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#f5f2f7',
+    primary: '#8352b7',
+    secondary: '#b381ea',
+    accent: '#9e48f9',
+  },
+}
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
+    theme: {
+      defaultTheme: 'customTheme',
+      themes: {
+        customTheme,
+      },
+    },
+
     // ... your configuration
   })
   app.vueApp.use(vuetify)
